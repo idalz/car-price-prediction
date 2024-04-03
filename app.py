b58c4695239f128320ef6c9b88b8eb7796c9d4ac
idalz <idalez@outlook.com>
2024-04-04 00:04:29 +0300

    removed debug parameter

--- app.py
+++ app.py
@@ -45,4 +45,5 @@ def about():
 
 if __name__ == "__main__":
     clApp = ClientApp()
-    app.run(host='0.0.0.0', port=8080, debug=True)
+    app.run(host='0.0.0.0', port=8080)
+    #app.run(host='0.0.0.0', port=8080, debug=True)
